77117ef9d99199e69d1762f49221198bfa169925
Charles Lynch <clynch19@gatech.edu>
2020-11-06 00:23:40 -0500

    Unfucking the dependencies

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Scott\Documents\Georgia Tech\Statistical Machine Learning\DurOrNoDur\.venv
+set VIRTUAL_ENV=D:\ML\DurOrNoDur\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
