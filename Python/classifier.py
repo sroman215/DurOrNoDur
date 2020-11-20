a6dffd4d104ff32514cf2742a6d864b3a3f548be
Charles Lynch <clynch19@gatech.edu>
2020-11-20 14:11:25 -0500

    Finished KNN classifier and part of SVM '

--- Python/classifier.py
+++ Python/classifier.py
@@ -1,3 +1,9 @@
+from sklearn import neighbors
+
+from sklearn import svm
+
+import numpy as np
+
 class Classifier:
     def __init__(self, data):
         self.data = data
@@ -10,37 +16,84 @@ class Classifier:
             if (value.label == label):
                 newDict[key] = value
         return newDict
+    
+    def constructDataSet(self, start,stop):
+        return [*list(self.durData.values())[start:stop], *list(self.noDurData.values())[start:stop]]
 
-    def KNNClassify(self,num_neighbors,image_train,image_test):
-        from sklearn import neighbors
-        KNNClassifier = neighbors.KNeighborsClassifier(n_neighbors=num_neighbors,algorithm="brute")
-        KNNClassifier.fit(image_train.featureVectors,image_train.label)
-        KNNClassifier.fit(image_train.featureVectors,image_train.label)
-        predicted_labels = KNNClassifier.predict(image_test.featureVectors)
-        predicted_empirical_values = (KNNClassifier.predict(image_train.label))
-        predicted_test_values =  (KNNClassifier.predict(test_set_data))
-        EMP_error=(predicted_empirical_values!=train_set_label).sum()
-        True_error = (predicted_test_values!=test_set_label).sum()
-        N = len(predicted_empirical_values)
-        N_t = len(predicted_test_values)
-        Empircal_risk_array = EMP_error/N
-        True_risk_array = True_error/N_t
+    def KNNClassify(self):
+        maxNeighbors = 10
+        Empircal_risk_array = [0 for x in range(maxNeighbors)]
+
+        True_risk_array = [0 for x in range(maxNeighbors)] 
+
         
+        for k in range(1, maxNeighbors):
+            num_neighbors = k
+            data_ratio_train = 0.65
+            cutoff = int(np.floor(data_ratio_train*len(self.data)*data_ratio_train))
+            test = len(self.data)
+            trainData = self.constructDataSet(0,cutoff)
+            lengthOfTrainData = len(trainData)
+            fvsTrain = list(map(lambda x: x.featureVectors, trainData))
+            labelsTrain = list(map(lambda x: x.label, trainData))
+            lengthOfTrainData = len(labelsTrain)
+            testData = self.constructDataSet(cutoff+1,int(np.floor(len(self.data))))
+            fvsTest = list(map(lambda x: x.featureVectors, testData))
+            labelsTest = list(map(lambda x: x.label, testData))
+            lengthOfTrainData = len(labelsTest)
+
+            print(k)
+
+            KNNClassifier = neighbors.KNeighborsClassifier(n_neighbors=num_neighbors,algorithm="brute")
+            KNNClassifier.fit(fvsTrain,labelsTrain)
+            predicted_labels = KNNClassifier.predict(fvsTest)
+            predicted_empirical_values = (KNNClassifier.predict(fvsTrain))
+            EMP_error=(predicted_empirical_values!=labelsTrain).sum()
+            True_error = (predicted_labels!=labelsTest).sum()
+            N = len(predicted_empirical_values)
+            N_t = len(predicted_labels)
+            Empircal_risk_array[k-1] = EMP_error/N
+            True_risk_array[k-1] = True_error/N_t
+            
         return np.array(Empircal_risk_array,True_risk_array)
 
-    def SVMClassify(self,image_train,image_test,kernel_string,gamma_const,C_const):
-        from sklearn.svm import SVC
-        svm = SVC(kernel=kernel_string, random_state=1, gamma=gamma_const, C=C_const)
-        svm.fit(image_train.featureVectors, image_train.label)
-        predicted_labels = svm.predict(image_test.featureVectors)
-        predicted_empirical_values = (svm.predict(image_train.label))
-        predicted_test_values =  (svm.predict(test_set_data))
-        EMP_error=(predicted_empirical_values!=train_set_label).sum()
-        True_error = (predicted_test_values!=test_set_label).sum()
-        N = len(predicted_empirical_values)
-        N_t = len(predicted_test_values)
-        Empircal_risk_array = EMP_error/N
-        True_risk_array = True_error/N_t
+    def SVMClassify(self):
+        gamma_const=0.001
+        C_const =10
+
+        kernel_string = ['rbf','linear']
+        Empircal_risk_array = [0 for x in range(len(kernel_string))]
+
+        True_risk_array = [0 for x in range(len(kernel_string))] 
+
+        data_ratio_train = 0.65
+        cutoff = int(np.floor(data_ratio_train*len(self.data)*data_ratio_train))
+        test = len(self.data)
+        trainData = self.constructDataSet(0,cutoff)
+        lengthOfTrainData = len(trainData)
+        fvsTrain = list(map(lambda x: x.featureVectors, trainData))
+        labelsTrain = list(map(lambda x: x.label, trainData))
+        lengthOfTrainData = len(labelsTrain)
+        testData = self.constructDataSet(cutoff+1,int(np.floor(len(self.data))))
+        fvsTest = list(map(lambda x: x.featureVectors, testData))
+        labelsTest = list(map(lambda x: x.label, testData))
+        lengthOfTrainData = len(labelsTest)
+
+        counter = 0
+
+        for k in kernel_string:
+
+            svmClass = svm.SVC(kernel=k, random_state=1, gamma=gamma_const, C=C_const,class_weight={1: 10})
+            svmClass.fit(fvsTrain, labelsTrain)
+            predicted_labels = svmClass.predict(fvsTest)
+            predicted_empirical_values = (svmClass.predict(fvsTrain))
+            EMP_error=(predicted_empirical_values!=labelsTrain).sum()
+            True_error = (predicted_labels!=labelsTest).sum()
+            N = len(predicted_empirical_values)
+            N_t = len(predicted_labels)
+            Empircal_risk_array[counter] = EMP_error/N
+            True_risk_array[counter] = True_error/N_t
+            counter = counter +1
         return np.array(Empircal_risk_array,True_risk_array)
 
 
--- Python/runner.py
+++ Python/runner.py
@@ -6,14 +6,14 @@ from classifier import Classifier
 ### Import the files and extract out the feature vectors/labels from the data
 myParser = ImageParser()
 loadFromMemory = True
-fileDict = myParser.loadImageDictToFile() if loadFromMemory else myParser.constructDictionary()
+fileDict = myParser.loadImageDictFromFile() if loadFromMemory else myParser.constructDictionary()
 
 ### Use the image data dictionary to classify the data
-myClassifier = Classifier(myParser.imageDict)
+myClassifier = Classifier(fileDict)
 
 ### Use the classifier to predict some data
-results = myClassifier.KNNClassify()
-
+#resultsKNN = myClassifier.KNNClassify()
+resultsSVM = myClassifier.SVMClassify() 
 ### Put the output of our classifier for the test data in a nice output
 myAnalysis = Analysis()
-myAnalysis.graphResults(results)
+myAnalysis.graphResults(resultsSVM)
